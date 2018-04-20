3155f9ad5d56a78b9d7ba92ea1102b0d6357d0e0
Onur Torna <onurtorna@gmail.com>
2018-04-20 16:43:06 +0300

    Change landing page view controller name

---
+++ RoomBooking/RoomBooking/Scenes/Landing/View/LandingPageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LandingPageViewController.swift
+//  RoomBooking
+//
+//  Created by Onur Torna on 19/04/18.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
