a7102f55d49bfa6e505fd8d2745cf55db71912f3
Onur Torna <onurtorna@gmail.com>
2018-05-02 14:08:02 +0300

    Add nibloadable protocol

---
+++ RoomBooking/RoomBooking/Protocols/NibLoadable.swift
@@ -0,0 +1,32 @@
+//
+//  NibLoadable.swift
+//  RoomBooking
+//
+//  Created by Onur Torna on 23/04/18.
+//
+
+import UIKit
+
+protocol NibLoadable {
+    static var defaultNibName: String { get }
+    static func loadFromNib() -> Self
+}
+
+extension NibLoadable where Self: UIView {
+
+    static var defaultNibName: String {
+        return String(describing: self)
+    }
+
+    static var defaultNib: UINib {
+        return UINib(nibName: defaultNibName, bundle: nil)
+    }
+
+    static func loadFromNib() -> Self {
+        guard let nib = Bundle.main.loadNibNamed(defaultNibName, owner: nil, options: nil)?.first as? Self else {
+            fatalError("[NibLoadable] Cannot load view from nib.")
+        }
+        return nib
+    }
+
+}
