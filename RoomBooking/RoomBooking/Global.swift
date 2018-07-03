e3f3922d888e288f7ef9474ede018be4c5bda42f
Onur Torna <onurtorna@gmail.com>
2018-07-03 22:59:46 +0300

    Add global enum

---
+++ RoomBooking/RoomBooking/Global.swift
@@ -0,0 +1,13 @@
+//
+//  Global.swift
+//  RoomBooking
+//
+//  Created by Onur Torna on 03/07/18.
+//
+
+import Foundation
+
+enum Global {
+    static let bookingStartHour = 7
+    static let openHourCount = 12
+}
